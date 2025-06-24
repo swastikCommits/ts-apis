d13aff02f515a536e0c63c42b06911834390bacf
swastikCommits <textswastik.alt@gmail.com>
2025-06-24 07:10:34 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ interface User {
     name: string,
     age: number
 }
+
+function sumOfAge(user1: User, user2: User) {
+    return user1.age + user2.age
+}
+const age = sumOfAge({name: 'John', age: 20}, {name: 'Jane', age: 30})
+console.log(age);
