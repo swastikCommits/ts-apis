49472305e2f298f9f971f06c3fd3c28f3b31b920
swastikCommits <textswastik.alt@gmail.com>
2025-06-24 07:33:42 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
-interface User {
-    name: string,
-    age: number
-}
+// interface User {
+//     name: string,
+//     age: number
+// }
 
-function sumOfAge(user1: User, user2: User) {
-    return user1.age + user2.age
-}
-const age = sumOfAge({name: 'John', age: 20}, {name: 'Jane', age: 30})
-console.log(age);
+// function sumOfAge(user1: User, user2: User) {
+//     return user1.age + user2.age
+// }
+// const age = sumOfAge({name: 'John', age: 20}, {name: 'Jane', age: 30})
+// console.log(age);
+
+
+
+
+
+
+
+// type NumberArray = readonly number[];
+// const a: NumberArray = [1,2,3];
+// a[0] = 4;
+// // using readonly it will throw error
+
+
+// const userName = 'John';
+// userName = 'Anuj';
+// // TS starts complaining while array didn't
+ 
+
